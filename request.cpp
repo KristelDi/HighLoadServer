88ae6d3e6f1946ecae72029f37c810a1ac770670
KristelDi <Dezi2004@rambler.ru>
2016-09-27 08:14:12 +0300

    get head

---
+++ request.cpp
@@ -0,0 +1,7 @@
+#include "request.h"
+
+request::request()
+{
+
+}
+
---
+++ request.h
@@ -0,0 +1,12 @@
+#ifndef REQUEST_H
+#define REQUEST_H
+
+#include <QObject>
+
+class request
+{
+public:
+    request();
+};
+
+#endif // REQUEST_H
